eca3a41a0cd220a0f7b3ecee878adbbf067c67f7
Steven Brills <stevenbrills@hotmail.com>
2021-12-28 10:22:19 -0800

    Completed spacing factorization and keyword estimation

--- code/python/code.py
+++ code/python/code.py
@@ -2,7 +2,7 @@ from random import sample
 import numpy as np
 
 #Load file
-abc_freq = [8.2, 1.5,2.8,4.3,13,2.2,2,6.1,7,0.15,0.77,4,2.4,6.7,7.5,1.9,0.095,6,6.3,9.1,2.8,0.98,2.4,0.15,2,0.074]
+abc_freq = np.array([8.2, 1.5,2.8,4.3,13,2.2,2,6.1,7,0.15,0.77,4,2.4,6.7,7.5,1.9,0.095,6,6.3,9.1,2.8,0.98,2.4,0.15,2,0.074])
 
 #Read in the source text
 source_text = open("../../source_text/cipher_text.txt","r")
@@ -28,11 +28,20 @@ print(search_word_length)
 #randomly pick a word of that length from the text
 sampling_set = [(text[i:i+search_word_length]) for i in range(0, len(text), search_word_length)]
 search_word_sample = sample(sampling_set, 1)[0]
+search_word_sample = "EEDC"
 print(search_word_sample)
+
 
 #check for repetitions and count the repetitions
 num_repetitions = sampling_set.count(search_word_sample)
 print(num_repetitions)
+
+ind1 = text.index(search_word_sample,0)
+ind2 = text.index(search_word_sample,ind1)
+
+print(ind1)
+print(ind2)
+
 
 #if repetitions more than 1, save it else discard
 if num_repetitions>1:
